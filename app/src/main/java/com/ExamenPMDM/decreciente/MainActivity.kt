93c5627343cbe702a67013e850b5bc7a8cc7151c
gabri185 <gdominguezreyes@danielcastelao.org>
2021-12-13 11:01:52 +0100

    Subiendo projecto

--- app/src/main/java/com/ExamenPMDM/decreciente/MainActivity.kt
+++ app/src/main/java/com/ExamenPMDM/decreciente/MainActivity.kt
@@ -2,10 +2,36 @@ package com.ExamenPMDM.decreciente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boton: Button = findViewById(R.id.boton)
+
+        boton.setOnClickListener() {
+            pulsar()
+
+
+        }
+        decreciente()
     }
+    fun decreciente(){
+        var numero:Int = 100;
+        for (i in 1..100){
+            Log.d("restar" , "Se resta el numero " + numero + "-1")
+            numero--
+        }
+    }
+    fun pulsar(){
+
+            Toast.makeText(this, "Pulsaste el boton", Toast.LENGTH_LONG).show()
+
+
+    }
+
 }
