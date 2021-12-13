eff502d388d242cbd259918a55793ca4ac5a64c2
gabri185 <gdominguezreyes@danielcastelao.org>
2021-12-13 09:44:11 +0100

    Initial commit

---
+++ app/src/main/java/com/ExamenPMDM/decreciente/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ExamenPMDM.decreciente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
